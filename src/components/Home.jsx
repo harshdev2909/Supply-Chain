146bfc4496c635bda16f236f6eea5ce30813c782
harshdev2909 <harsh.dev2909@gmail.com>
2025-01-03 12:59:26 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,14 @@ import Hero from "./Hero"
 import Features from "./Features"
 import Contact from "./Contact"
 import Footer from "./Footer"
+
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
         <Hero />
-        <Features />
+        <Features />     
         <Contact />
       </main>
       <Footer />
